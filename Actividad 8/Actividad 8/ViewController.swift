c367a211cc5f541c0bf39cfd42f014fe2baa9387
Glendapps <78885940+Glendapps@users.noreply.github.com>
2021-03-23 11:49:43 -0600

    Add files via upload

---
+++ Actividad 8/Actividad 8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Actividad 8
+//
+//  Created by user185903 on 3/23/21.
+//  Copyright © 2021 user185903. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
